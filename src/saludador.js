ca8491cca00d9cd83131a508d301b69750993a82
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2026-02-18 23:17:20 -0400

    feat: agregar saludo segun la hora del dia

--- src/saludador.js
+++ src/saludador.js
@@ -1,5 +1,17 @@
 function saludar(nombre) {
-  return "Hola " + nombre;
+  const hora = new Date().getHours();
+
+  let saludo;
+
+  if (hora < 12) {
+    saludo = "Buenos días";
+  } else if (hora < 18) {
+    saludo = "Buenas tardes";
+  } else {
+    saludo = "Buenas noches";
+  }
+
+  return saludo + " " + nombre;
 }
 
 export default saludar;
