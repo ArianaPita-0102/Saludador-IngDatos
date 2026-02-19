8dd32078499f5785d79cd596a464460b11570984
ArianaPita-0102 <ariana.pita@ucb.edu.bo>
2026-02-18 23:15:18 -0400

    feat: saludar con nombre de usuario

--- src/presenter.js
+++ src/presenter.js
@@ -2,9 +2,12 @@ import saludar from "./saludador.js";
 
 const form = document.querySelector("#saludar-form");
 const div = document.querySelector("#resultado-div");
+const nombreInput = document.querySelector("#nombre");
 
 form.addEventListener("submit", (event) => {
   event.preventDefault();
 
-  div.innerHTML = "<p>" + saludar() + "</p>";
+  const nombre = nombreInput.value;
+
+  div.innerHTML = "<p>" + saludar(nombre) + "</p>";
 });
